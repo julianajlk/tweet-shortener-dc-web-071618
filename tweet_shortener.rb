59055220f3d5859d6e6e0935360f70ac55aa05a0
Juliana Kang <julianajlk@gmail.com>
2018-07-13 14:46:55 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -41,7 +41,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
-    word_substituter(tweet)[0..140]
+    word_substituter(tweet)[0..137]+ "..."
   else
     word_substituter(tweet)
 end
