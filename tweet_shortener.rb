13751e9d0ae5f9443d2698707780c7b92f748b29
Juliana Kang <julianajlk@gmail.com>
2018-07-13 13:24:29 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ end
 def word_substituter(tweet)
   words = tweet.split(" ")
   shortened_tweet = []
-    tweet.split.collect do |word|
+    words.collect do |word|
       if dictionary.keys.include?(word.downcase)
         word = dictionary[word.downcase]
       else
