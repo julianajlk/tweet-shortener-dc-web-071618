77c269b719387cebac089636b80f88fde095b414
Juliana Kang <julianajlk@gmail.com>
2018-07-13 13:03:57 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,21 @@
 # Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
 def word_substituter(tweet)
   words = tweet.split(" ")
   shortened_word = []
+  words.each do |word|
 end
